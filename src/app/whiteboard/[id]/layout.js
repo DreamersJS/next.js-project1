0ea902773eaa655ac68d56fada0c3485f97f5de1
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2025-11-16 20:34:07 +0200

    fix: image wasn't displaying
    move socket provider to inner layout

--- src/app/whiteboard/[id]/layout.js
+++ src/app/whiteboard/[id]/layout.js
@@ -1,23 +1,26 @@
 'use client';
 import { AuthProvider } from "@/context/AuthProvider";
+import { SocketProvider } from "@/context/SocketProvider";
 import dynamic from 'next/dynamic';
 const ChatToggleWrapper = dynamic(() => import('@/components/ChatToggleWrapper'), { ssr: false });
 
 export default function WhiteboardLayout({ children }) {
-// <SocketProvider></SocketProvider> here instead of in the other layout?
-  return (
-      <AuthProvider>
-        <div className="flex flex-row h-screen overflow-hidden">
-          {/* Main Content - Whiteboard */}
-          <main className="flex-grow h-full bg-white p-0 flex items-center justify-center overflow-hidden">
-            {children}
-          </main>
 
-          {/* Right Rail - Chat */}
-          <aside>
-            <ChatToggleWrapper />
-          </aside>
-        </div>
-      </AuthProvider>
+  return (
+    <AuthProvider>
+      <div className="flex flex-row h-screen overflow-hidden">
+        {/* Main Content - Whiteboard */}
+        <main className="flex-grow h-full bg-white p-0 flex items-center justify-center overflow-hidden">
+          <SocketProvider>
+            {children}
+          </SocketProvider>
+        </main>
+
+        {/* Right Rail - Chat */}
+        <aside>
+          <ChatToggleWrapper />
+        </aside>
+      </div>
+    </AuthProvider>
   );
 }
